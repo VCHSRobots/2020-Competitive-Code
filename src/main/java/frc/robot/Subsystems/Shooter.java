cd93167e9f49a8921e1358809c0e0a3a774d1329
ctaylor22 <conner.taylor8@gmail.com>
2020-01-18 14:07:20 -0800

    Got Shooter started

--- src/main/java/frc/robot/Subsystems/Shooter.java
+++ src/main/java/frc/robot/Subsystems/Shooter.java
@@ -1,11 +1,21 @@
 package frc.robot.Subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
 import frc.robot.Robot;
+import frc.robot.RobotMap;
+import frc.robot.util.BaseFXConfig;
 
 public class Shooter {
+    //-----Falcon Motors-------
+    WPI_TalonFX upperWheelsFX;
+    WPI_TalonFX lowerWheelsFX;
+    WPI_TalonFX turnTableFX;
 
     public void robotInit() {
-
+        upperWheelsFX = BaseFXConfig.generateDefaultTalon(RobotMap.ShooterMap.kupperWheelsFX);
+        lowerWheelsFX = BaseFXConfig.generateDefaultTalon(RobotMap.ShooterMap.klowerWheelsFX);
+        turnTableFX = BaseFXConfig.generateDefaultTalon(RobotMap.ShooterMap.kturnTableFX);
     }
 
     public void robotPeriodic() {
