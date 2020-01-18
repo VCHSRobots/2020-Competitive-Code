97ffc410321d601fb1c7e17c8d16a3a86a53ad8e
ctaylor22 <conner.taylor8@gmail.com>
2020-01-18 13:43:34 -0800

    sakl

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -3,11 +3,12 @@ package frc.robot;
 public class RobotMap {
 
     public static class Controllers {
-        public static int kDriveCtrl = 0; //These are port numbers
+        public static int kDriveCtrl = 0; // These are port numbers
         public static int kManipCtrl = 1;
     }
 
     public static class ColorWheelMap {
+        public static int controlPanelWheel = 2;
 
     }
 
@@ -16,20 +17,6 @@ public class RobotMap {
         public static int kIntakeFalconMotor = 22;
         public static int kUpDownForward = 23;
         public static int kUpDownReverse = 24;
-    }
-
-    public static class ShooterMap {
-
-    }
-
-    public static class DriveTrainMap {
-    }
-
-        public static int controlPanelWheel = 2;
-    }
-
-    public static class IntakeMap {
-
     }
 
     public static class ShooterMap {
