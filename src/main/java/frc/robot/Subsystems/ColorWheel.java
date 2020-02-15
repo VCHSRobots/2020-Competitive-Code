e265713ea39db90cff36b27ba8bfbdd9ae97035b
Caleb <caleb.wojo@outlook.com>
2020-02-15 12:54:28 -0800

    Update ColorWheel.java

--- src/main/java/frc/robot/Subsystems/ColorWheel.java
+++ src/main/java/frc/robot/Subsystems/ColorWheel.java
@@ -12,10 +12,8 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
-
 import frc.robot.RobotMap;
 import frc.robot.util.FMSData;
-import frc.robot.Constants;
 import frc.robot.ControllerMap;
 import frc.robot.Robot;
 
@@ -23,38 +21,25 @@ import com.revrobotics.ColorSensorV3;
 import com.revrobotics.ColorMatchResult;
 import com.revrobotics.ColorMatch;
 
-import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
+
+/**
+ * This is a simple example to show how the REV Color Sensor V3 can be used to
+ * detect pre-configured colors.
+ */
 
 public class ColorWheel {
 
-    private TalonFXConfiguration m_falconSettings = new TalonFXConfiguration();
     TalonFX falcon;
 
-<<<<<<< Updated upstream
-    final const k_RPMToSensorVelocity = 2047.0/600.0;
-    final const k_sensorVelocityToRPM = 600.0/2047.0;
     XboxController xbox;
 
-<<<<<<< Updated upstream
-=======
-=======
-    final double k_RPMToSensorVelocity = 2047.0/600.0;
-    final double k_sensorVelocityToRPM = 600.0/2047.0;
-    XboxController xbox;
-
->>>>>>> Stashed changes
-    int controlPanelRotationTicks = 49152;
-
->>>>>>> Stashed changes
     double RPM = 0;
 
+    boolean colorCheck = false;
     boolean rotateDisk = false;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
     
     String currentColor = "Unknown";
     String fmsColor;
@@ -63,19 +48,6 @@ public class ColorWheel {
 
     private FMSData fmsColorData = new FMSData();
     private ColorSensorV3 m_colorSensor;
-=======
-=======
->>>>>>> Stashed changes
-
-    String colorString = "Unknown";
-    String fmsColorString;
-
-    private FMSData fmsColor = new FMSData();
-    private ColorSensorV3 m_colorSensor = null;
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     private final I2C.Port i2cPort = I2C.Port.kOnboard;
     private final ColorMatch m_colorMatcher = new ColorMatch();
     private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
@@ -91,42 +63,12 @@ public class ColorWheel {
         m_colorMatcher.addColorMatch(kRedTarget);
         m_colorMatcher.addColorMatch(kYellowTarget);
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
         colorSolenoid = new DoubleSolenoid(RobotMap.ColorWheelMap.kcolorSolenoidForward,RobotMap.ColorWheelMap.kcolorSolenoidReverse);
-=======
-=======
->>>>>>> Stashed changes
-        m_falconSettings.voltageCompSaturation = 11;
-        m_falconSettings.supplyCurrLimit = new SupplyCurrentLimitConfiguration(true, 15, 15, 0.2);
-        m_falconSettings.openloopRamp = 0.03; 
-        m_falconSettings.forwardSoftLimitEnable = false;
-        m_falconSettings.reverseSoftLimitEnable = false;
-        m_falconSettings.neutralDeadband = 0.03;
-        m_falconSettings.nominalOutputForward = 0;
-        m_falconSettings.nominalOutputReverse = 0;
-        m_falconSettings.peakOutputForward = 1;
-        m_falconSettings.peakOutputReverse = -1;
-        m_falconSettings.closedloopRamp = 0.03;
-        m_falconSettings.slot0.allowableClosedloopError = 0;
-        m_falconSettings.slot0.closedLoopPeakOutput = 1.0;
-        m_falconSettings.slot0.closedLoopPeriod = 2;
-        m_falconSettings.slot0.integralZone = 0;
-        m_falconSettings.slot0.kP = 0;
-        m_falconSettings.slot0.kI = 0;
-        m_falconSettings.slot0.kD = 0;
-        m_falconSettings.slot0.kF = 0.03;
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 
         falcon = new TalonFX(RobotMap.ColorWheelMap.kcontrolPanelWheel);
         falcon.configFactoryDefault();
 
         falcon.setSelectedSensorPosition(0);
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
         // falcon.config_kP(0, 0);
         // falcon.config_kI(0, 0);
         // falcon.config_kD(0, 0);
@@ -140,25 +82,10 @@ public class ColorWheel {
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-=======
-        falcon.configAllSettings(m_falconSettings);
-
-        xbox = Robot.manipCtrl;
-
-        colorTry();
->>>>>>> Stashed changes
-=======
-        falcon.configAllSettings(m_falconSettings);
-
-        xbox = Robot.manipCtrl;
-
-        colorTry();
->>>>>>> Stashed changes
     }
 
     public void robotPeriodic() {
-        
-        colorTry();
+
         Color detectedColor = m_colorSensor.getColor();
         ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
         SmartDashboard.putNumber("encoder", falcon.getSelectedSensorPosition());
@@ -166,16 +93,8 @@ public class ColorWheel {
         SmartDashboard.putNumber("Green", detectedColor.green);
         SmartDashboard.putNumber("Blue", detectedColor.blue);
         SmartDashboard.putNumber("Confidence", match.confidence);
-<<<<<<< Updated upstream
         SmartDashboard.putString("Detected Color", currentColor);
         RPM = SmartDashboard.getNumber("RPM of ColorWheel", 0);
-=======
-        SmartDashboard.putString("Detected Color", colorString);
-        RPM = SmartDashboard.getNumber("disk RPM", 0);
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 
     }
 
@@ -201,23 +120,7 @@ public class ColorWheel {
 
     public void teleopPeriodic() {
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
         //Double velocityPer100Milliseconds = RPM * 4096 / 600;
-=======
-=======
->>>>>>> Stashed changes
-        colorTry();
-        if (fmsColor.toString() != null) {
-            fmsColorString = fmsColor.toString();
-        }
-
-        boolean yButton = xbox.getRawButton(ControllerMap.Manip.krotateButton);
-        boolean endButton = xbox.getRawButton(ControllerMap.Manip.koperatedRotation);
-        boolean startButton = xbox.getRawButton(ControllerMap.Manip.knearestColor);
-
-        Double velocityPer100Milliseconds = RPM * 4096 / 600;
->>>>>>> Stashed changes
         int encoderTicks = falcon.getSelectedSensorPosition();
         Color detectedColor = m_colorSensor.getColor();
         ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
@@ -249,18 +152,7 @@ public class ColorWheel {
         } else if (match.color == kYellowTarget && match.confidence >= 0.94) {
             currentColor = "Yellow";
         } else {
-<<<<<<< Updated upstream
             currentColor = "Unknown";
-=======
-            colorString = "Unknown";
-        }
-
-        // control to manually move hand
-        if (startButton) {
-            falcon.set(ControlMode.Velocity, velocityPer100Milliseconds);
-        } else {
-            falcon.set(ControlMode.Velocity, 0);
->>>>>>> Stashed changes
         }
 
         // control to rotate disk three times
@@ -279,21 +171,10 @@ public class ColorWheel {
         }
 
         // Enters Finding the Color Mode through FMS
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
         if (xbox.getBButton()) {
             // if fmsColor is blue and currentColor isnt red then move until then
             if (fmsColor == "blue" && currentColor != "Red") {
                 falcon.set(ControlMode.PercentOutput, 0.20);
-=======
-=======
->>>>>>> Stashed changes
-        if (endButton) {
-           colorTry();
-            // if fmsColor is blue and colorString isnt red then move until then
-            if (fmsColorString == "blue" && colorString != "Red") {
-                falcon.set(ControlMode.Velocity, velocityPer100Milliseconds);
->>>>>>> Stashed changes
                 return;
                 // if fmsColor is green and currentColor isnt yellow then move until then
             } else if (fmsColor == "green" && currentColor != "Yellow") {
@@ -327,13 +208,5 @@ public class ColorWheel {
 
     public void teleopDisabled() {
 
-    }
-
-    private void colorTry() {
-        try {
-            m_colorSensor = new ColorSensorV3(i2cPort);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
     }
 }
