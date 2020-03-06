ceba17a4f8ed626e35f01d0722986d80cd0ae8a6
ctaylor22 <conner.taylor8@gmail.com>
2020-03-05 23:15:49 -0800

    end of thurs

--- src/main/java/frc/robot/Subsystems/Intake.java
+++ src/main/java/frc/robot/Subsystems/Intake.java
@@ -1,5 +1,6 @@
 package frc.robot.Subsystems;
 
+import frc.robot.Constants;
 import frc.robot.ControllerMap;
 import frc.robot.Robot;
 import frc.robot.RobotMap.IntakeMap;
@@ -14,7 +15,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Intake {
   // motors
-  WPI_TalonSRX intakeBagMotor = BaseSRXConfig.generateDefaultTalon(IntakeMap.kIntakeBagMotor);
+  WPI_TalonSRX intakeMotor;
+  BaseSRXConfig m_config = new BaseSRXConfig();
 
   DoubleSolenoid intakeSolenoidTop = new DoubleSolenoid(IntakeMap.kPCM, IntakeMap.kTopForward, IntakeMap.kTopReverse);
   DoubleSolenoid intakeSolenoidBottom = new DoubleSolenoid(IntakeMap.kPCM, IntakeMap.kBottomForward, IntakeMap.kBottomReverse);
@@ -29,7 +31,27 @@ public class Intake {
   double intakeSpeed = 0.5;
 
   public void robotInit() {
-    intakeBagMotor.setInverted(true);
+    m_config.voltageCompSaturation = Constants.kvoltageComp;
+    m_config.openloopRamp = 0.03;
+    m_config.forwardSoftLimitEnable = false;
+    m_config.reverseSoftLimitEnable = false;
+    m_config.neutralDeadband = 0.03;
+    m_config.nominalOutputForward = 0;
+    m_config.nominalOutputReverse = 0;
+    m_config.peakOutputForward = 1;
+    m_config.peakOutputReverse = -1;
+    m_config.closedloopRamp = 0.03;
+    m_config.slot0.allowableClosedloopError = 0;
+    m_config.slot0.closedLoopPeakOutput = 1.0;
+    m_config.slot0.closedLoopPeriod = 2;
+    m_config.slot0.integralZone = 0;
+    m_config.slot0.kP = 0;
+    m_config.slot0.kI = 0;
+    m_config.slot0.kD = 0;
+    m_config.slot0.kF = 0.0;
+    intakeMotor = new WPI_TalonSRX(IntakeMap.kIntakeBagMotor);
+    intakeMotor.configAllSettings(m_config);
+    intakeMotor.setInverted(true);
 
     intakeSolenoidTop.set(DoubleSolenoid.Value.kReverse);
     intakeSolenoidBottom.set(DoubleSolenoid.Value.kReverse);
@@ -68,12 +90,12 @@ public class Intake {
       intakeToggle = !intakeToggle;
     }
 
-    if (intakeToggle) {
-      intakeBagMotor.set(intakeSpeed);
-    } else {
-      intakeBagMotor.set(0);
-    }
-    // intakeBagMotor.set(ControlMode.PercentOutput, intakeSpeed); // for prototype intake
+    // if (intakeToggle) {
+    //   intakeMotor.set(intakeSpeed);
+    // } else {
+    //   intakeMotor.set(0);
+    // }
+    intakeMotor.set(ControlMode.PercentOutput, intakeSpeed); // for prototype intake
 
     // pneumatic position
 
